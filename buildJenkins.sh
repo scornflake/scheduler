5d2298f192e71d03bf00d013cc51f42b7a014c45
Neil Clayton <neil@shinywhitebox.com>
2018-07-14 09:50:35 +1200

    shifted fastlane config to the root

--- buildJenkins.sh
+++ buildJenkins.sh
@@ -1,14 +1,14 @@
 export PATH="${WORKSPACE}/node_modules/.bin:$PATH"
 
+# Will do both normal and dev dependencies
 yarn install
-yarn install --dev
 
 npm rebuild node-sass
 npm run build:prod
 
-"${WORKSPACE}/buildDockerImage.sh"
-
 ionic cordova prepare ios --release --prod
 ionic cordova prepare android --release --prod
 
+# Get ourselves a docker container of the /www code
+"${WORKSPACE}/buildDockerImage.sh"
 
