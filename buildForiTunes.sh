bf84678b3e2e892329d1e4be03778fbaea1a9512
Neil Clayton <neil@shinywhitebox.com>
2018-07-16 11:37:32 +1200

    wheee - works to upload APK on android

--- buildForiTunes.sh
+++ buildForiTunes.sh
@@ -5,11 +5,8 @@ export PATH="${WORKSPACE}/node_modules/.bin:$PATH"
 
 # Will do both normal and dev dependencies
 yarn install
-
 npm rebuild node-sass
 
-ionic cordova resources ios
-ionic cordova build ios --prod --release
 fastlane ios beta
 
 
