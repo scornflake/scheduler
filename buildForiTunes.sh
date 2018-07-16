9388cd70f4f5d0fa35f71151e552df149267a30e
Neil Clayton <neil@shinywhitebox.com>
2018-07-16 13:42:44 +1200

    build build build build build build build build build build build build build build build build

--- buildForiTunes.sh
+++ buildForiTunes.sh
@@ -2,6 +2,7 @@
 set -e
 
 export PATH="$HOME/.fastlane/bin:${WORKSPACE}/node_modules/.bin:$PATH"
+export ANDROID_HOME="$HOME/Library/Android/sdk"
 
 # Will do both normal and dev dependencies
 yarn install
