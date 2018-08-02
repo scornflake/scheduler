d5a65411e977b6711a04859ecd313e650bc2b79c
Neil Clayton <neil@shinywhitebox.com>
2018-08-03 09:01:10 +1200

    check for service

--- src/providers/mapping/setup.ts
+++ src/providers/mapping/setup.ts
@@ -1,7 +1,11 @@
 import {scheduler_db_map} from "../../assets/db.mapping";
 import {OrmMapper} from "./orm-mapper";
+import {LoggingService} from "ionic-logging-service";
 
-export function setupOrmMapper(logService): OrmMapper {
+export function setupOrmMapper(logService: LoggingService): OrmMapper {
+    if (logService == null) {
+        throw new Error('No LoggingService provided?!');
+    }
     let mapper = new OrmMapper(logService);
     mapper.addConfiguration(scheduler_db_map);
     return mapper;
